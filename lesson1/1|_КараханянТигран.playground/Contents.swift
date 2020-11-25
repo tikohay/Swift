f69650c312fd0648cf25378324d53eb495b84747
Karakhanyan Tigran <lev.98@bk.ru>
2020-11-25 18:15:31 +0300

    lesson 1 is done

---
+++ lesson1/1|_КараханянТигран.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var a: Int = 3
+var b: Int = 4
+var x: Int = 2
+var c: Int = 5
+var res: Int = 0
+
+res = (a * Int(pow(Double(x), 2))) + (b * x) + c
+
+print("результат квадратного уравнения равен", res)
+    
